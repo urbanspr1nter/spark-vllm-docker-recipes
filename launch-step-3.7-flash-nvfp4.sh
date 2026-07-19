0cbd4c90da9f437c90507d12cd3dde9aeca3ec3f
Roger Ngo <rngo@netflix.com>
2026-07-18 19:36:26 -0700

    Step-3.7-Flash-NVFP4

---
+++ launch-step-3.7-flash-nvfp4.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+HOST_1=192.168.100.21
+HOST_2=192.168.100.22
+BASE_PATH="$HOME/code/spark-vllm-docker"
+
+"$BASE_PATH/run-recipe.sh" step-3.7-flash-nvfp4 \
+    --no-ray \
+    --ib-if rocep1s0f0 \
+    --eth-if enp1s0f0np0 \
+    -n $HOST_1,$HOST_2 
