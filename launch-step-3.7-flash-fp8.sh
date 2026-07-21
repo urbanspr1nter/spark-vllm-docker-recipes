f7a5297fa4274ff68b7cfd2c627262e2bfb41732
Roger Ngo <rngo@netflix.com>
2026-07-21 15:51:20 -0700

    Launch Step-3.7-Flash-FP8

---
+++ launch-step-3.7-flash-fp8.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+HOST_1=192.168.100.23
+HOST_2=192.168.100.24
+BASE_PATH="$HOME/code/spark-vllm-docker"
+
+"$BASE_PATH/run-recipe.sh" step-3.7-flash-fp8 \
+    --no-ray \
+    --ib-if rocep1s0f0 \
+    --eth-if enp1s0f0np0 \
+    --max-num-seqs 2 \
+    -n $HOST_1,$HOST_2 
