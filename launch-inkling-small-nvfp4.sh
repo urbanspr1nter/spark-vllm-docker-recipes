7f6db531ffc7d864e521c64ef24b8374ac8b0c97
Roger Ngo <rngo@netflix.com>
2026-08-09 20:21:24 -0700

    launch scripts

--- launch-inkling-small-nvfp4.sh
+++ launch-inkling-small-nvfp4.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-HOST_1=192.168.100.23
-HOST_2=192.168.100.24
+HOST_1=192.168.100.21
+HOST_2=192.168.100.22
 BASE_PATH="$HOME/code/spark-vllm-docker"
 
-"$BASE_PATH/run-recipe.sh" qwen3.5-122b-a10b-fp8 \
-    --no-ray \
-    --early-oom \
+"$BASE_PATH/run-recipe.sh" inkling-small-nvfp4 \
     --ib-if rocep1s0f0 \
     --eth-if enp1s0f0np0 \
+    --max-num-seqs 2 \
     -n $HOST_1,$HOST_2 
--- launch-qwen3.5-397b-a17b-fp8.sh
+++ launch-qwen3.5-397b-a17b-fp8.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-HOST_1=192.168.100.21
-HOST_2=192.168.100.22
-HOST_3=192.168.100.23
-HOST_4=192.168.100.24
+HOST_1=192.168.100.23
+HOST_2=192.168.100.24
+HOST_3=192.168.100.25
+HOST_4=192.168.100.26
 BASE_PATH="$HOME/code/spark-vllm-docker"
 
 "$BASE_PATH/run-recipe.sh" $BASE_PATH/recipes/4x-spark-cluster/qwen3.5-397b-a17B-fp8 \
     --no-ray \
     --ib-if rocep1s0f0 \
     --eth-if enp1s0f0np0 \
-    --earlyoom --earlyoom-args "-M 2097152,524288 -s 100 -r 60" \
+    --max-num-seqs 8 \
+    --gpu-memory-utilization 0.88 \
+    --earlyoom --earlyoom-args "-M 262144,102400 -s 100 -r 60" \
     -n $HOST_1,$HOST_2,$HOST_3,$HOST_4
