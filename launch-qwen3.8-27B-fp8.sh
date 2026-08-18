9640a6cd0b44cb62cc50d2ba3957a44ff73a293f
Roger Ngo <rngo@netflix.com>
2026-08-18 15:07:19 -0700

    Qwen3.8-27B-FP8 TP2

--- launch-qwen3.8-27B-fp8.sh
+++ launch-qwen3.8-27B-fp8.sh
@@ -4,8 +4,7 @@ HOST_1=192.168.100.27
 HOST_2=192.168.100.28
 BASE_PATH="$HOME/code/spark-vllm-docker"
 
-"$BASE_PATH/run-recipe.sh" deepseek-v4-flash-0731-abliterated \
-    --max-model-len 524288 \
+"$BASE_PATH/run-recipe.sh" qwen3.8-27B-fp8 \
     --ib-if rocep1s0f0 \
     --eth-if enp1s0f0np0 \
     -n $HOST_1,$HOST_2 
